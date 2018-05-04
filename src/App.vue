b799fdb311f4fea77ef21b561d2e292cd2fda864
Kody Wang <553630288@qq.com>
2018-05-04 20:18:29 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  text-align: center;
+}
+</style>
